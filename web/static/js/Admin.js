2dc1deafafd19877b4fb3c3eb3582991e236951c
peopleig <patelprachet@gmail.com>
2025-08-13 14:35:25 +0530

    Fix Admin page bugs

--- web/static/js/Admin.js
+++ web/static/js/Admin.js
@@ -10,3 +10,37 @@ document.querySelectorAll('.approve-btn').forEach(btn => {
         }
     });
 });
+const rows = document.querySelectorAll("#orders_table_body tr");
+const filter_buttons = document.querySelectorAll(".order_buttons");
+filter_buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const status = btn.dataset.status;
+        rows.forEach(row => {
+            const rowStatus = row.dataset.status;
+            if (status === "all" || rowStatus === status) {
+                row.classList.remove("d-none");
+            } else {
+                row.classList.add("d-none");
+            }
+        });
+        filter_buttons.forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+    });
+});
+
+const item_filter_buttons = document.querySelectorAll(".item_buttons");
+item_filter_buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const filter = button.dataset.chef;
+        document.querySelectorAll("#items_table_body tr").forEach(row => {
+            const chef_id = parseInt(row.dataset.chef_id);
+            row.style.display = (
+                filter === "all" ||
+                (filter === "unassigned" && chef_id === 1) ||
+                (filter === "assigned" && chef_id !== 1)
+            ) ? "table-row" : "none";
+        });
+        item_filter_buttons.forEach(b => b.classList.remove("active"));
+        button.classList.add("active");
+    });
+});
