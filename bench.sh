b09589a49c852bf7dd954fa9ffb35347ef014bae
peopleig <patelprachet@gmail.com>
2025-08-18 00:04:30 +0530

    Add backend stress test scripts

---
+++ bench.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source bench.env
+
+# curl -i -b "$COOKIE_FILE" "$BASE_URL/menu"
+echo "--------------------------------------------------"
+echo "Running GET /menu benchmark with cookies..."
+echo "--------------------------------------------------"
+ab -n 100000 -c 1000 \
+  -C "jwt_token=$(grep token $COOKIE_FILE | awk '{print $NF}')" \
+  "$BASE_URL/menu"
+
+echo "--------------------------------------------------"
+echo "Running POST /menu benchmark with cookies..."
+echo "--------------------------------------------------"
+
+echo '{
+  "cart": [
+    {
+      "quantity": 2,
+      "itemId": 1
+    },
+    {
+      "quantity": 1,
+      "itemId": 2
+    }
+  ],
+  "special_instructions": "No onion no garlic",
+  "order_type": "takeaway",
+  "table_number": "7"
+}' > payload.json
+
+ab -n 100000 -c 100 \
+  -T application/json \
+  -p payload.json \
+  -C "jwt_token=$(grep token $COOKIE_FILE | awk '{print $NF}')" \
+  "$BASE_URL/menu"
+
+rm payload.json
